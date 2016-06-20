e1557b4e817696f4626351ee179cd39c8f00aaf5
Doug Clinton <doug@wiredthing.com>
2016-06-20 14:02:21 +0100

    Added test for ValueClassWrites

---
+++ src/test/scala/com/wellfactored/playbindings/ValueClassWritesTest.scala
@@ -0,0 +1,19 @@
+package com.wellfactored.playbindings
+
+import org.scalatest.{FlatSpec, Matchers}
+import play.api.libs.json.{JsNumber, Writes}
+
+object testClasses {
+  case class LongWrapper(l: Long) extends AnyVal
+}
+
+class ValueClassWritesTest extends FlatSpec with Matchers with ValueClassWrites {
+  import testClasses.LongWrapper
+
+  "writes" should "implicitly summon a Writes for LongWrapper" in {
+    val w = implicitly[Writes[LongWrapper]]
+
+    w.writes(LongWrapper(1337)) shouldBe JsNumber(1337)
+  }
+
+}
