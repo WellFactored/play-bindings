7ac9c45e0088ceda51b2d3626f622108de13a924
Doug Clinton <doug@wellfactored.com>
2016-12-27 18:41:21 +0000

    Restored the use of `value-wrappers` because the direct use of shapeless Generic proved unstable, either giving errors with diverging implicits or going into infinite loops during compilation

--- src/main/scala/com/wellfactored/playbindings/ValueClassFormats.scala
+++ src/main/scala/com/wellfactored/playbindings/ValueClassFormats.scala
@@ -17,24 +17,27 @@
 
 package com.wellfactored.playbindings
 
+import com.wellfactored.valuewrapper.{ValueWrapper, ValueWrapperGen}
 import play.api.libs.json._
-import shapeless.Generic
 
-trait ValueClassReads extends GenericValueWrapper {
+trait ValueClassReads extends ValueWrapperGen {
   implicit def genericReads[W, V](implicit
-                                  gen: Generic.Aux[W, V],
+                                  vw: ValueWrapper[W, V],
                                   rv: Reads[V]): Reads[W] = new Reads[W] {
-    override def reads(json: JsValue): JsResult[W] = rv.reads(json).map { v =>
-      gen.from(v)
+    override def reads(json: JsValue): JsResult[W] = rv.reads(json).flatMap { v =>
+      vw.wrap(v) match {
+        case Left(e) => JsError(e)
+        case Right(w) => JsSuccess(w)
+      }
     }
   }
 }
 
-trait ValueClassWrites extends GenericValueWrapper {
+trait ValueClassWrites extends ValueWrapperGen {
   implicit def genericWrites[W, V](implicit
-                                   gen: Generic.Aux[W, V],
+                                   vw: ValueWrapper[W, V],
                                    wv: Writes[V]): Writes[W] = new Writes[W] {
-    override def writes(w: W): JsValue = wv.writes(gen.to(w))
+    override def writes(w: W): JsValue = wv.writes(vw.unwrap(w))
   }
 }
 
--- src/main/scala/com/wellfactored/playbindings/ValueClassUrlBinders.scala
+++ src/main/scala/com/wellfactored/playbindings/ValueClassUrlBinders.scala
@@ -17,34 +17,36 @@
 
 package com.wellfactored.playbindings
 
+import com.wellfactored.valuewrapper.{ValueWrapper, ValueWrapperGen}
 import play.api.mvc.{PathBindable, QueryStringBindable}
-import shapeless.Generic
 
-trait ValueClassPathBindable extends GenericValueWrapper {
-  implicit def valueClassPathBindable[W, V](implicit gen: Generic.Aux[W, V],
+trait ValueClassPathBindable extends ValueWrapperGen {
+  implicit def valueClassPathBindable[W, V](implicit vw: ValueWrapper[W, V],
                                             binder: PathBindable[V]): PathBindable[W] =
     new PathBindable[W] {
       override def unbind(key: String, wrapper: W): String =
-        binder.unbind(key, gen.to(wrapper))
+        binder.unbind(key, vw.unwrap(wrapper))
 
       override def bind(key: String, value: String): Either[String, W] =
-        binder.bind(key, value).right.map(gen.from)
+        for {
+          v1 <- binder.bind(key, value).right
+          v2 <- vw.wrap(v1).right
+        } yield v2
     }
 }
 
-trait ValueClassQueryStringBindable extends GenericValueWrapper {
-  implicit def valueClassQueryStringBindable[W, V](implicit gen: Generic.Aux[W, V],
+trait ValueClassQueryStringBindable extends ValueWrapperGen {
+  implicit def valueClassQueryStringBindable[W, V](implicit vw: ValueWrapper[W, V],
                                                    binder: QueryStringBindable[V]): QueryStringBindable[W] =
     new QueryStringBindable[W] {
       override def unbind(key: String, wrapper: W): String =
-        binder.unbind(key, gen.to(wrapper))
+        binder.unbind(key, vw.unwrap(wrapper))
 
       override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, W]] = {
-        binder.bind(key, params).map(_.right.map(gen.from))
+        binder.bind(key, params).map(_.right.flatMap(vw.wrap))
       }
     }
 }
-
 
 trait ValueClassUrlBinders extends ValueClassPathBindable with ValueClassQueryStringBindable
 
